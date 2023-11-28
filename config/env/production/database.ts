61ef67e6178d740e97c9faea621f6fdf195490be
mateudev <mateuszbogacz.dev@gmail.com>
2023-11-28 19:44:25 +0100

    add arguments to config database

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", "5432"),
+      database: env("DATABASE_NAME", "dbhorses"),
+      user: env("DATABASE_USERNAME", "dbhorses"),
+      password: env("DATABASE_PASSWORD", "dbhorses"),
       ssl: {
         ca: env("DATABASE_CA"),
       },
