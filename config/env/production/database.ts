42ac60a92ef1bb410cd2c143c012e21ee0250008
mateudev <mateuszbogacz.dev@gmail.com>
2023-11-28 20:24:45 +0100

    add pg-connextion-string

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,14 +1,20 @@
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", "5432"),
-      database: env("DATABASE_NAME", "dbhorses"),
-      user: env("DATABASE_USERNAME", "dbhorses"),
-      password: env("DATABASE_PASSWORD", "dbhorses"),
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: {
-        ca: env("DATABASE_CA", false),
+        ca: env("DATABASE_CA"),
       },
     },
     debug: false,
