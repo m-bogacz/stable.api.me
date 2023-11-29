060da629cd2d6210c4dac638753941dd80169585
mateudev <mateuszbogacz.dev@gmail.com>
2023-11-29 17:48:09 +0100

    enabled playground graphql

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,10 +1,11 @@
 export default {
   //
   graphql: {
+    enabled: true,
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
