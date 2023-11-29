98d18b8ddee5b95af17f1aa5a4cf71d5de0c0ac1
mateudev <mateuszbogacz.dev@gmail.com>
2023-11-29 17:51:17 +0100

    add plugin graphql

---
+++ config/plugins.ts
@@ -0,0 +1,16 @@
+export default {
+  //
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
